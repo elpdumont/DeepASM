3769cc9eaf66d90bc6f73e68bb82115410c34c5b
Emmanuel Dumont <elp.dumont@gmail.com>
2024-05-02 12:25:15 -0400

    Ordered CpGs in order sample, chr, region_inf

--- app/prepare-samples/python/find_nb_states_for_HMM.py
+++ app/prepare-samples/python/find_nb_states_for_HMM.py
@@ -115,7 +115,7 @@ def main():
     quoted_samples = ",".join(
         [f"'{sample}'" for sample in samples_dic[dataset_for_hmm]]
     )
-    query = f"SELECT {hmm_var} FROM {project}.{ml_dataset}.features_wo_hmm WHERE sample IN ({quoted_samples}) AND {hmm_var} IS NOT NULL"
+    query = f"SELECT {hmm_var} FROM {project}.{ml_dataset}.features_wo_hmm WHERE sample IN ({quoted_samples}) AND {hmm_var} IS NOT NULL ORDER BY sample, chr, region_inf"
     # Execute the query and store in dic
     df = bq_client.query(query).to_dataframe()
     df["cpg_directional_fm"] = df["cpg_directional_fm"].apply(
