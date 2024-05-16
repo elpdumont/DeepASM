fdb072faa5bcdc5fa1afc0f88878592a9aec4718
Emmanuel Dumont <elp.dumont@gmail.com>
2024-05-15 21:32:53 -0400

    Threshold 0.8

--- main.sh
+++ main.sh
@@ -20,7 +20,7 @@ us-east1-docker.pkg.dev/hmh-em-deepasm/docker-repo/bash:"${SHORT_SHA}" \
 docker run -it \
 -v ~/.config/gcloud/application_default_credentials.json:/appuser/.config/gcloud/application_default_credentials.json:ro \
 -e GOOGLE_APPLICATION_CREDENTIALS=/appuser/.config/gcloud/application_default_credentials.json \
-us-east1-docker.pkg.dev/hmh-em-deepasm/docker-repo/python:"3d08d96" \
+us-east1-docker.pkg.dev/hmh-em-deepasm/docker-repo/python:"9f17e4a" \
 /bin/bash
 
 
@@ -115,6 +115,42 @@ echo "The percentage difference in the number of rows is ${percentageDifference}
 
 
 
+
+
+#---------------------------------------------
+# PREPARE FEATURES FOR ML. REQUIRE THE JSON FILES OUT THE CLOUDASM SCRIPT (FORMAT CLOUDASM TO STANDARD REGIONS)
+
+echo "Preparing the features for all the regions (even if they do not have ASM flagged)"
+NB_FILES_PER_TASK="50"
+NB_FILES_TO_PROCESS=$(gsutil ls gs://"${BUCKET}"/"${DATA_PATH}"/after_cloudasm/all_regions/* | wc -l | awk '{print $1}')
+TASK_COUNT=$(( (${NB_FILES_TO_PROCESS} + ${NB_FILES_PER_TASK} - 1) / ${NB_FILES_PER_TASK} ))
+sed -i '' "s#NB_FILES_PER_TASK_PH#${NB_FILES_PER_TASK}#g" "batch-jobs/prepare_features_for_ML.json"
+sed -i '' "s#TASK_COUNT_PH#${TASK_COUNT}#g" "batch-jobs/prepare_features_for_ML.json"
+
+
+gcloud batch jobs submit "prepare-features-for-ml-${SHORT_SHA}" \
+	--location "${REGION}" \
+	--config batch-jobs/prepare_features_for_ML.json
+
+
+nb_regions=$(execute_query "SELECT COUNT(*) FROM ${PROJECT}.${ML_DATASET}.features_wo_hmm")
+nb_regions_w_data=$(execute_query "SELECT COUNT(*) FROM ${PROJECT}.${ML_DATASET}.features_wo_hmm WHERE cpg_directional_fm IS NOT NULL ")
+nb_regions_w_data_and_asm_flagged=$(execute_query "SELECT COUNT(*) FROM ${PROJECT}.${ML_DATASET}.features_wo_hmm WHERE cpg_directional_fm IS NOT NULL AND asm IS NOT NULL")
+echo "Features were prepared for ${nb_regions} regions. Among these, we could extract features for ${nb_regions_w_data} regions. Among these, we have ${nb_regions_w_data_and_asm_flagged} regions with features and ASM."
+
+echo "Exporting the dataset with features (excluding HMM) to the bucket"
+bq extract --destination_format=NEWLINE_DELIMITED_JSON "${PROJECT}:${ML_DATASET}.features_wo_hmm" gs://"${BUCKET}"/"${DATA_PATH}"/features_wo_hmm/features_wo_hmm_*.json
+
+
+
+
+#---------------------------------------------
+# FIT TRANSFORMER AND RNN
+
+gcloud batch jobs submit "transformer-rnn-1d-${SHORT_SHA}" \
+	--location "${REGION}" \
+	--config batch-jobs/run_transformer_and_rnn_1d.json
+
 #---------------------------------------------
 # Find the number of states that works best
 NB_STATES="10"
@@ -161,6 +197,3 @@ gcloud batch jobs submit "tree-search-${SHORT_SHA}" \
 	--config batch-jobs/perform_random_search_tree.json
 
 
-gcloud batch jobs submit "transformer-rnn-1d-${SHORT_SHA}" \
-	--location "${REGION}" \
-	--config batch-jobs/run_transformer_and_rnn_1d.json
